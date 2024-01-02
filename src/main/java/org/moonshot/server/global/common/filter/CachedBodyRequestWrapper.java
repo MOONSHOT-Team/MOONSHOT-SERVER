1033a760bddf33c961ae581243ffa7655a9b0d97
its-sky <smc9919@naver.com>
2024-01-03 03:47:43 +0900

    [Feat] #7 : HttpRequest Body 추출을 위한 Filter 및 클래스 구현

---
+++ src/main/java/org/moonshot/server/global/common/filter/CachedBodyRequestWrapper.java
@@ -0,0 +1,36 @@
+package org.moonshot.server.global.common.filter;
+
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.springframework.util.StreamUtils;
+
+public class CachedBodyRequestWrapper extends HttpServletRequestWrapper {
+
+    private final byte[] cachedBody;
+
+    public CachedBodyRequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        this.cachedBody = StreamUtils.copyToByteArray(request.getInputStream());
+    }
+
+    @Override
+    public ServletInputStream getInputStream() {
+        return new CachedBodyServletInputStream(this.cachedBody);
+    }
+
+    @Override
+    public BufferedReader getReader() {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
+        return new BufferedReader(new InputStreamReader(byteArrayInputStream));
+    }
+
+
+    public String getBody() {
+        return new String(cachedBody);
+    }
+}
