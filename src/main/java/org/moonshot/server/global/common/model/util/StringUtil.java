bf62ba967d8a9f51aa0fc6811f68f44eb15d6512
its-sky <smc9919@naver.com>
2024-01-03 03:45:51 +0900

    [Feat] #7 : RequestBody String 전처리 Util 클래스

---
+++ src/main/java/org/moonshot/server/global/common/model/util/StringUtil.java
@@ -0,0 +1,85 @@
+package org.moonshot.server.global.common.model.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StringUtil {
+
+    public static String translateEscapes(String str) {
+        if (str.isEmpty()) {
+            return "";
+        }
+        char[] chars = str.toCharArray();
+        int length = chars.length;
+        int from = 0;
+        int to = 0;
+        while (from < length) {
+            char ch = chars[from++];
+            if (ch == '\\') {
+                ch = from < length ? chars[from++] : '\0';
+                switch (ch) {
+                    case 'b':
+                        ch = '\b';
+                        break;
+                    case 'f':
+                        ch = '\f';
+                        break;
+                    case 'n':
+                        ch = '\n';
+                        break;
+                    case 'r':
+                        ch = '\r';
+                        break;
+                    case 's':
+                        ch = ' ';
+                        break;
+                    case 't':
+                        ch = '\t';
+                        break;
+                    case '\'':
+                    case '\"':
+                    case '\\':
+                        // as is
+                        break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                        int limit = Integer.min(from + (ch <= '3' ? 2 : 1), length);
+                        int code = ch - '0';
+                        while (from < limit) {
+                            ch = chars[from];
+                            if (ch < '0' || '7' < ch) {
+                                break;
+                            }
+                            from++;
+                            code = (code << 3) | (ch - '0');
+                        }
+                        ch = (char)code;
+                        break;
+                    case '\n':
+                        continue;
+                    case '\r':
+                        if (from < length && chars[from] == '\n') {
+                            from++;
+                        }
+                        continue;
+                    default: {
+                        String msg = String.format(
+                                "Invalid escape sequence: \\%c \\\\u%04X",
+                                ch, (int)ch);
+                        throw new IllegalArgumentException(msg);
+                    }
+                }
+            }
+
+            chars[to++] = ch;
+        }
+        return new String(chars, 0, to);
+    }
+}
