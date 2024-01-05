1c968c147626fbb82a42ba4b9922723650cb2241
its-sky <smc9919@naver.com>
2024-01-06 01:26:05 +0900

    [Fix] #24 : 컨벤션 수정

--- src/main/java/org/moonshot/server/domain/objective/exception/ObjectiveNotFoundException.java
+++ src/main/java/org/moonshot/server/domain/objective/exception/ObjectiveNotFoundException.java
@@ -4,7 +4,9 @@ import org.moonshot.server.global.common.exception.MoonshotException;
 import org.moonshot.server.global.common.response.ErrorType;
 
 public class ObjectiveNotFoundException extends MoonshotException {
+
     public ObjectiveNotFoundException() {
         super(ErrorType.NOT_FOUND_OBJECTIVE_ERROR);
     }
+
 }
