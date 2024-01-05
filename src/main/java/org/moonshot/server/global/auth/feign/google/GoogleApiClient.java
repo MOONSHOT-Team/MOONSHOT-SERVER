71417ea8997e83995ca81418a7c7cf75adfe01df
Choi YoungLyn <joeuni0129@gmail.com>
2024-01-06 03:10:10 +0900

    [Add] #26 - 카카오/구글로그인 FeignClient 추가

---
+++ src/main/java/org/moonshot/server/global/auth/feign/google/GoogleApiClient.java
@@ -0,0 +1,15 @@
+package org.moonshot.server.global.auth.feign.google;
+
+import org.moonshot.server.domain.user.dto.response.google.GoogleInfoResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "GoogleApiClient", url = "https://www.googleapis.com")
+public interface GoogleApiClient {
+    @GetMapping("/oauth2/v3/userinfo")
+    GoogleInfoResponse googleInfo(
+            @RequestHeader("Authorization") String token
+    );
+
+}
---
+++ src/main/java/org/moonshot/server/global/auth/feign/kakao/KakaoApiClient.java
@@ -0,0 +1,14 @@
+package org.moonshot.server.global.auth.feign.kakao;
+
+import org.moonshot.server.domain.user.dto.response.kakao.KakaoUserResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "kakaoApiClient", url = "https://kapi.kakao.com")
+public interface KakaoApiClient {
+
+    @GetMapping(value = "/v2/user/me")
+    KakaoUserResponse getUserInformation(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken);
+}
