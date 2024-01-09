680318cc9531522975fd1ce45086bf4c900dd4f6
Choi YoungLyn <joeuni0129@gmail.com>
2024-01-09 17:44:45 +0900

    [Chore] #39 - LogCreateRequestDto 변경

--- src/main/java/org/moonshot/server/domain/log/dto/request/LogCreateRequestDto.java
+++ src/main/java/org/moonshot/server/domain/log/dto/request/LogCreateRequestDto.java
@@ -2,14 +2,9 @@ package org.moonshot.server.domain.log.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.moonshot.server.domain.log.model.LogState;
 
 public record LogCreateRequestDto(
-        //만들어둔 KeyResultModitfyRequest는 어디서 수정하는건지 .. ?
-        Long keyresultId,
-
-        @NotNull(message = "체크인 종류를 입력해주세요.")
-        LogState logState,
+        Long keyResultId,
 
         @NotNull(message = "Log의 수치를 입력해주세요.")
         @Size(min = 1, max = 30, message = "진척정도의 수치 값은 30자 이하여야 합니다.")
