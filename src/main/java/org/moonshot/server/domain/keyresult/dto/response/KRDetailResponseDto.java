a066a4502089b99351a8650b34f78ee10ea18973
Choi YoungLyn <joeuni0129@gmail.com>
2024-01-10 05:19:05 +0900

    [Feat] #44 - KR조회 관련 ResponseDto 구현

--- src/main/java/org/moonshot/server/domain/keyresult/dto/response/KRDetailResponseDto.java
+++ src/main/java/org/moonshot/server/domain/keyresult/dto/response/KRDetailResponseDto.java
@@ -4,19 +4,20 @@ import org.moonshot.server.domain.keyresult.model.KRState;
 import org.moonshot.server.domain.log.dto.response.LogResponseDto;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public record KRDetailResponseDto(
         String title,
         int progressBar,
-        KRState krState,
-        LocalDateTime startAt,
-        LocalDateTime expireAt,
+        String krState,
+        String startAt,
+        String expireAt,
         List<LogResponseDto> logList
 ) {
 
-    public static KRDetailResponseDto of(String title, int progressBar, KRState krState, LocalDateTime startAt, LocalDateTime expireAt, List<LogResponseDto> logList) {
-        return new KRDetailResponseDto(title, progressBar, krState, startAt, expireAt, logList);
+    public static KRDetailResponseDto of(String title, int progressBar, String krState, LocalDateTime startAt, LocalDateTime expireAt, List<LogResponseDto> logList) {
+        return new KRDetailResponseDto(title, progressBar, krState, startAt.format(DateTimeFormatter.ISO_LOCAL_DATE), expireAt.format(DateTimeFormatter.ISO_LOCAL_DATE), logList);
     }
 
 }
--- src/main/java/org/moonshot/server/domain/log/dto/response/LogResponseDto.java
+++ src/main/java/org/moonshot/server/domain/log/dto/response/LogResponseDto.java
@@ -3,15 +3,17 @@ package org.moonshot.server.domain.log.dto.response;
 import org.moonshot.server.domain.log.model.LogState;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record LogResponseDto(
-        LogState state,
-        LocalDateTime date,
+        String logState,
+        String dateTime,
         String title,
         String content
 ) {
-    public static LogResponseDto of(LogState state, LocalDateTime date, String title, String content) {
-        return new LogResponseDto(state, date, title, content);
+    public static LogResponseDto of(String logState, LocalDateTime dateTime, String title, String content) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return new LogResponseDto(logState, dateTime.format(formatter), title, content);
     }
 
 }
