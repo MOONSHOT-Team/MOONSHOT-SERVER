c8351e9b6f34a4be47a96afe1128961ecd065522
its-sky <smc9919@naver.com>
2024-01-09 17:18:17 +0900

    [Add] #40 : OKR 생성 관련 API 유저 인가 로직 추가

--- src/main/java/org/moonshot/server/domain/task/controller/TaskController.java
+++ src/main/java/org/moonshot/server/domain/task/controller/TaskController.java
@@ -3,9 +3,11 @@ package org.moonshot.server.domain.task.controller;
 import static org.moonshot.server.global.common.response.SuccessType.*;
 
 import jakarta.validation.Valid;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.moonshot.server.domain.task.dto.request.TaskSingleCreateRequestDto;
 import org.moonshot.server.domain.task.service.TaskService;
+import org.moonshot.server.global.auth.jwt.JwtTokenProvider;
 import org.moonshot.server.global.common.response.ApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +22,8 @@ public class TaskController {
     private final TaskService taskService;
 
     @PostMapping
-    public ApiResponse<?> createTask(@RequestBody @Valid TaskSingleCreateRequestDto request) {
-        taskService.createTask(request);
+    public ApiResponse<?> createTask(Principal principal, @RequestBody @Valid TaskSingleCreateRequestDto request) {
+        taskService.createTask(request, JwtTokenProvider.getUserIdFromPrincipal(principal));
         return ApiResponse.success(POST_TASK_SUCCESS);
     }
 
