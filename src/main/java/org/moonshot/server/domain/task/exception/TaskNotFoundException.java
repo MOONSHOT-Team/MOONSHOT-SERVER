0c3d240d27051b486a0d21d362799250d3c08881
its-sky <smc9919@naver.com>
2024-01-09 22:42:49 +0900

    [Add] #42 : Task 순서 변경 bulk updating으로 구현

---
+++ src/main/java/org/moonshot/server/domain/task/exception/TaskNotFoundException.java
@@ -0,0 +1,10 @@
+package org.moonshot.server.domain.task.exception;
+
+import org.moonshot.server.global.common.exception.MoonshotException;
+import org.moonshot.server.global.common.response.ErrorType;
+
+public class TaskNotFoundException extends MoonshotException {
+    public TaskNotFoundException() {
+        super(ErrorType.NOT_FOUND_TASK_ERROR);
+    }
+}
