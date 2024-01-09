c7b2a12bae5efb95f17dec463238238a9ae5000e
its-sky <smc9919@naver.com>
2024-01-09 19:33:46 +0900

    [Add] #42 : index 수정 쿼리 Dirty Checking 방식이 아닌 Bulk Update 방식을 이용한 쿼리수 최적화

--- src/main/java/org/moonshot/server/domain/keyresult/repository/KeyResultRepository.java
+++ src/main/java/org/moonshot/server/domain/keyresult/repository/KeyResultRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.moonshot.server.domain.keyresult.model.KeyResult;
 import org.moonshot.server.domain.objective.model.Objective;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -13,8 +14,14 @@ public interface KeyResultRepository extends JpaRepository<KeyResult, Long> {
     Long countAllByObjective(Objective objective);
     List<KeyResult> findByIdIn(List<Long> ids);
     List<KeyResult> findAllByObjective(Objective objective);
+    List<KeyResult> findAllByObjectiveOrderByIdx(Objective objective);
     List<KeyResult> findAllByObjectiveId(Long objectiveId);
     @Query("select kr from KeyResult kr join fetch kr.objective join fetch kr.objective.user where kr.id = :keyResultId")
     Optional<KeyResult> findKeyResultAndObjective(@Param("keyResultId") Long keyResultId);
-
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE KeyResult kr SET kr.idx = kr.idx + 1 WHERE kr.idx >= :lBound AND kr.idx < :uBound AND kr.objective.id = :objectiveId AND kr.id != :targetId")
+    void bulkUpdateIdxIncrease(@Param("lBound") int lowerBound, @Param("uBound") int upperBound, @Param("objectiveId") Long objectiveId, @Param("targetId") Long targetId);
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE KeyResult kr SET kr.idx = kr.idx - 1 WHERE kr.idx >= :lBound AND kr.idx <= :uBound AND kr.objective.id = :objectiveId AND kr.id != :targetId")
+    void bulkUpdateIdxDecrease(@Param("lBound") int lowerBound, @Param("uBound") int upperBound, @Param("objectiveId") Long objectiveId, @Param("targetId") Long targetId);
 }
