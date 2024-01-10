7decdb431bece0638cf8deddc5ab06e48fe5abeb
its-sky <smc9919@naver.com>
2024-01-11 03:33:06 +0900

    [Feat] #48 : 메인 대시보드 조회 API 추가

--- src/main/java/org/moonshot/server/domain/objective/controller/ObjectiveController.java
+++ src/main/java/org/moonshot/server/domain/objective/controller/ObjectiveController.java
@@ -4,11 +4,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.moonshot.server.domain.objective.dto.request.OKRCreateRequestDto;
+import org.moonshot.server.domain.objective.dto.response.DashboardResponseDto;
 import org.moonshot.server.domain.objective.service.ObjectiveService;
 import org.moonshot.server.global.auth.jwt.JwtTokenProvider;
 import org.moonshot.server.global.common.response.ApiResponse;
 import org.moonshot.server.global.common.response.SuccessType;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +37,12 @@ public class ObjectiveController {
     public ApiResponse<?> deleteObjective(Principal principal, @PathVariable("objectiveId") Long objectiveId) {
         objectiveService.deleteObjective(JwtTokenProvider.getUserIdFromPrincipal(principal), objectiveId);
         return ApiResponse.success(SuccessType.DELETE_OBJECTIVE_SUCCESS);
+    }
+
+    @GetMapping
+    public ApiResponse<DashboardResponseDto> getObjectiveInDashboard(Principal principal) {
+        DashboardResponseDto response = objectiveService.getObjectiveInDashboard(JwtTokenProvider.getUserIdFromPrincipal(principal));
+        return ApiResponse.success(SuccessType.GET_OKR_LIST_SUCCESS, response);
     }
 
 }
