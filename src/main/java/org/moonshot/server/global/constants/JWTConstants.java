e6f4c5b496d52887fa599b0611a68fdf4ee0e902
Choi YoungLyn <joeuni0129@gmail.com>
2024-01-10 22:07:02 +0900

    [Chore] #50 - 엑세스 토큰 만료시간 2주로 변경

--- src/main/java/org/moonshot/server/global/constants/JWTConstants.java
+++ src/main/java/org/moonshot/server/global/constants/JWTConstants.java
@@ -3,7 +3,7 @@ package org.moonshot.server.global.constants;
 public class JWTConstants {
 
     public static final String USER_ID = "userId";
-    public static final Long ACCESS_TOKEN_EXPIRATION_TIME = 60 * 1000L * 20;  // 액세스 토큰 만료 시간: 20분으로 지정
-    public static final Long REFRESH_TOKEN_EXPIRATION_TIME = 60 * 1000L * 60 * 24 * 7 * 2;  // 리프레시 토큰 만료 시간: 2주로 지정
+    public static final Long ACCESS_TOKEN_EXPIRATION_TIME = 60 * 1000L * 60 * 24 * 7 * 2;
+    public static final Long REFRESH_TOKEN_EXPIRATION_TIME = 60 * 1000L * 60 * 24 * 7 * 2; 
 
 }
