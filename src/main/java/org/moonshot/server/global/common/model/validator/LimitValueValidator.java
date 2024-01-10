dc227b1146fa43421af3e95df8104bb04af8689f
its-sky <smc9919@naver.com>
2024-01-11 02:46:12 +0900

    [Fix] #48 : 어노테이션 코드 오작성 수정

--- src/main/java/org/moonshot/server/global/common/model/validator/LimitValueValidator.java
+++ src/main/java/org/moonshot/server/global/common/model/validator/LimitValueValidator.java
@@ -3,7 +3,7 @@ package org.moonshot.server.global.common.model.validator;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class LimitValueValidator implements ConstraintValidator<ValidTargetNumber, Long> {
+public class LimitValueValidator implements ConstraintValidator<ValidLimitValue, Long> {
 
     private final long MAX_LIMIT = 99999999999L;
 
