18a877a1846e68f381346758027e1454a5bbce5a
its-sky <smc9919@naver.com>
2024-01-15 20:03:05 +0900

    [Fix] #99 - SwaggerConfig JWT Access Token 제대로 인식 되도록 변경

--- src/main/java/org/moonshot/server/global/config/SwaggerConfig.java
+++ src/main/java/org/moonshot/server/global/config/SwaggerConfig.java
@@ -1,37 +1,40 @@
 package org.moonshot.server.global.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        name = "JWT Auth",
-        in = SecuritySchemeIn.HEADER,
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer",
-        description = "Authorization: Bearer ~"
-)
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI(){
-        Info info = new Info()
-                .title("MOONSHOT API Document") // 말 그대로 스웨거의 제목
-                .version("V1") // 표기될 버전( 버전을 쓰는 방법에 대해서는 개발자 모두가 한번 알아보면 좋을거 같아요!!)
-                .description("MOONSHOT API 명세서입니다.");  // 제목에 대한 설명입니다!
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
         return new OpenAPI()
-                .addServersItem(new Server().url("/")) //초기
-                .components(new Components())
-                .info(info);
+                .components(components)
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("MOONSHOT API Document")
+                .version("V1")
+                .description("MOONSHOT API 명세서입니다.");
     }
 
 }
