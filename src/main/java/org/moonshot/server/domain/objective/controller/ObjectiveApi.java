ef45edac3f5ef3d274da8ecf3877707890446eb3
its-sky <smc9919@naver.com>
2024-01-15 20:27:14 +0900

    [Fix] #99 - Objective 관련 API Swagger 설정 인터페이스 설명에 . 삭제

--- src/main/java/org/moonshot/server/domain/objective/controller/ObjectiveApi.java
+++ src/main/java/org/moonshot/server/domain/objective/controller/ObjectiveApi.java
@@ -26,20 +26,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 interface ObjectiveApi {
 
     @ApiResponses(value =  {
-            @ApiResponse(responseCode = "201", description = "O-KR을 생성을 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "201", description = "O-KR을 생성을 성공하였습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
             @ApiResponse(responseCode = "400", description = "허용된 Objective 개수를 초과하였습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 유저입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class)))
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 유저입니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class)))
     })
     @Operation(summary = "O-KR 데이터 생성")
     ResponseEntity<MoonshotResponse<?>> createObjective(@Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "Access Token", required = true, schema = @Schema(type = "string")) Principal principal,
                                                         @RequestBody @Valid OKRCreateRequestDto request);
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "O-KR 트리 삭제를 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "403", description = "해당 자원에 접근 권한이 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 Objective입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class)))
+            @ApiResponse(responseCode = "204", description = "O-KR 트리 삭제를 성공하였습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "403", description = "해당 자원에 접근 권한이 없습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 Objective입니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class)))
     })
     @Operation(summary = "O-KR 데이터 삭제")
     ResponseEntity<?> deleteObjective(@Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "Access Token", required = true, schema = @Schema(type = "string")) Principal principal,
@@ -47,20 +47,20 @@ interface ObjectiveApi {
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Objective 수정에 성공하였습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Objective 종료 기간은 오늘보다 이전 날짜일 수 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "403", description = "해당 자원에 접근 권한이 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 Objective입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class)))
+            @ApiResponse(responseCode = "400", description = "Objective 종료 기간은 오늘보다 이전 날짜일 수 없습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "403", description = "해당 자원에 접근 권한이 없습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 Objective입니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class)))
     })
     @Operation(summary = "Objective 데이터 수정")
     ResponseEntity<MoonshotResponse<?>> modifyObjective(@Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "Access Token", required = true, schema = @Schema(type = "string")) Principal principal,
                                                         @RequestBody ModifyObjectiveRequestDto request);
 
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "로그인에 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "403", description = "해당 자원에 접근 권한이 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 Objective입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class)))
+            @ApiResponse(responseCode = "200", description = "O-KR 목록 조회에 성공하였습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "403", description = "해당 자원에 접근 권한이 없습니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 Objective입니다", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MoonshotResponse.class)))
     })
     @Operation(summary = "O-KR 목록 조회")
     ResponseEntity<MoonshotResponse<DashboardResponseDto>> getObjectiveInDashboard(@Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "Access Token", required = true, schema = @Schema(type = "string")) Principal principal,
