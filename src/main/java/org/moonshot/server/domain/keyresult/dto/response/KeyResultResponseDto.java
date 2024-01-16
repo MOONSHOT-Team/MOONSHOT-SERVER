ead27b7fe001045f34b24e4d838ac7261cc8223b
its-sky <smc9919@naver.com>
2024-01-16 15:33:38 +0900

    [Fix] #110 - OKR 조회 API KeyResult Title 변경

--- src/main/java/org/moonshot/server/domain/keyresult/dto/response/KeyResultResponseDto.java
+++ src/main/java/org/moonshot/server/domain/keyresult/dto/response/KeyResultResponseDto.java
@@ -13,7 +13,7 @@ public record KeyResultResponseDto(
     public static List<KeyResultResponseDto> of(List<KeyResult> krList) {
         return krList.stream().map(kr -> new KeyResultResponseDto(
                 kr.getId(),
-                kr.getTitle(),
+                kr.getTitle() + ": " + kr.getTarget() + kr.getMetric(),
                 kr.getIdx(),
                 TaskResponseDto.of(kr.getTaskList())
         )).toList();
