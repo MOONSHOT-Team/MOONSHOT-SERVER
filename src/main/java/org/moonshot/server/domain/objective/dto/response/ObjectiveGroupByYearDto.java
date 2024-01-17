940d8526d465a7ba41a978b8cf9da98d2eda6519
its-sky <smc9919@naver.com>
2024-01-17 12:21:18 +0900

    [Del] #123 - 연도별 개수 Dto 내 위경 변

--- src/main/java/org/moonshot/server/domain/objective/dto/response/ObjectiveGroupByYearDto.java
+++ src/main/java/org/moonshot/server/domain/objective/dto/response/ObjectiveGroupByYearDto.java
@@ -1,19 +1,15 @@
 package org.moonshot.server.domain.objective.dto.response;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 import org.moonshot.server.domain.objective.model.Objective;
 
 public record ObjectiveGroupByYearDto(
         int year,
-        int count,
         List<HistoryObjectiveListDto> objList
 ) {
     public static ObjectiveGroupByYearDto of(Integer year, List<Objective> objList) {
         return new ObjectiveGroupByYearDto(
                 year,
-                objList.size(),
                 objList.stream().map(HistoryObjectiveListDto::of).toList());
     }
 }
