825b4906c1ba067226931f6b92a21ac93de9a8d8
Choi YoungLyn <joeuni0129@gmail.com>
2024-01-19 15:48:06 +0900

    [Fix] #166 - 디스코드 알림 안오는 이슈 해결

--- src/main/java/org/moonshot/server/global/constants/DiscordConstants.java
+++ src/main/java/org/moonshot/server/global/constants/DiscordConstants.java
@@ -1,10 +1,7 @@
 package org.moonshot.server.global.constants;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public class DiscordConstants {
 
-    @Value("${logging.discord.signin.webhook-uri}")
-    public static String signInWebhookUrl;
+    public static final String signInWebhookUrl = "https://discord.com/api/webhooks/1197531070241968261/h18e24N_r18WGafR6syjSVX8568oO6grRY6jt21V2_vy-LVuy47jVUmMnB-sTFx3CZ-S";
 
 }
