c21e27cdd8714a9b02699da171355c570cfc2305
Choi YoungLyn <joeuni0129@gmail.com>
2024-01-23 01:46:46 +0900

    [Fix] #189 - 디스코드 알림에 사용자 프로필 추가

--- src/main/java/org/moonshot/server/global/external/discord/DiscordAppender.java
+++ src/main/java/org/moonshot/server/global/external/discord/DiscordAppender.java
@@ -118,13 +118,14 @@ public class DiscordAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
         }
     }
 
-    public void signInAppend(String name, String email, String socialPlatform, LocalDateTime createdAt){
+    public void signInAppend(String name, String email, String socialPlatform, LocalDateTime createdAt, String imgUrl){
         DiscordWebHook discordWebhook = new DiscordWebHook(signInWebhookUrl, username, avatarUrl, false);
 
         discordWebhook.addEmbed(new EmbedObject()
                 .setTitle("🚀[회원 가입] 새로운 유저가 가입하였습니다.🚀")
                 .setColor(Color.CYAN)
                 .setDescription("moonshot에 새로운 유저가 가입하였습니다.")
+                .setThumbnail(imgUrl)
                 .addField("[이름]", name, false)
                 .addField("[이메일]", email, false)
                 .addField("[소셜 플랫폼]", socialPlatform, false)
