066c6fa77c8720f60b53d717037d1c342ef27a0b
its-sky <smc9919@naver.com>
2024-01-29 22:17:38 +0900

    [Fix] #193 - RedisTemplate 빈 주입 충돌로 인하여 @Primary 추가

--- moonshot-auth/src/main/java/org/moonshot/config/RedisConfig.java
+++ moonshot-auth/src/main/java/org/moonshot/config/RedisConfig.java
@@ -1,8 +1,9 @@
-package org.moonshot.server.global.config;
+package org.moonshot.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -25,6 +26,7 @@ public class RedisConfig {
     }
 
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
