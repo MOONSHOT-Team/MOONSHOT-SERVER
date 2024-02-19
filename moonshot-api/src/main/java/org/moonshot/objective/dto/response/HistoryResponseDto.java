3652ed2fa7eba3dd4caf61b6ea823f5051d12cff
its-sky <smc9919@naver.com>
2024-02-19 16:52:54 +0900

    [Del] #200 - Dto 불필요한 코드 삭제

--- moonshot-api/src/main/java/org/moonshot/objective/dto/response/HistoryResponseDto.java
+++ moonshot-api/src/main/java/org/moonshot/objective/dto/response/HistoryResponseDto.java
@@ -7,7 +7,6 @@ import org.moonshot.objective.model.Criteria;
 public record HistoryResponseDto(
         List<ObjectiveGroupByYearDto> groups,
         List<YearDto> years,
-//        Map<Integer, Integer> years,
         List<String> categories
 ) {
     public static HistoryResponseDto of(List<ObjectiveGroupByYearDto> groups, Map<Integer, Integer> years,
