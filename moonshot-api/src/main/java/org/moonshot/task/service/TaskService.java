703e5e5c0b8719f3169691d9d9d129cf34bb2c54
Choi YoungLyn <joeuni0129@gmail.com>
2024-02-20 15:41:52 +0900

    [Feat] #202 - Task 삭제 API 구현

--- moonshot-api/src/main/java/org/moonshot/task/service/TaskService.java
+++ moonshot-api/src/main/java/org/moonshot/task/service/TaskService.java
@@ -94,4 +94,10 @@ public class TaskService implements IndexService {
         return (taskCount <= idx) || (idx < 0);
     }
 
+    public void deleteTask(final Long userId, long taskId) {
+        Task task = taskRepository.findTaskWithFetchJoin(taskId)
+                        .orElseThrow(TaskNotFoundException::new);
+        userService.validateUserAuthorization(task.getKeyResult().getObjective().getUser(), userId);
+        taskRepository.deleteById(taskId);
+    }
 }
