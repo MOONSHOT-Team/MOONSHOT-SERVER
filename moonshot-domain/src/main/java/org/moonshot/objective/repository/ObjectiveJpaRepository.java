52c0baa0f72a0ced86443f3187c9f1134f6a664d
its-sky <smc9919@naver.com>
2024-03-12 18:49:04 +0900

    [Del] #248 - ObjectiveJpaRepository 사용하지 않는 시그니처 삭제

--- moonshot-domain/src/main/java/org/moonshot/objective/repository/ObjectiveJpaRepository.java
+++ moonshot-domain/src/main/java/org/moonshot/objective/repository/ObjectiveJpaRepository.java
@@ -11,7 +11,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface ObjectiveJpaRepository extends JpaRepository<Objective, Long> {
 
-    Long countAllByUserAndIsClosed(User user, boolean isClosed);
     @Query("select o from Objective o join fetch o.user where o.id = :objective_id")
     Optional<Objective> findObjectiveAndUserById(@Param("objective_id") Long objectiveId);
     @Query("select distinct o from Objective o left join fetch o.keyResultList kr left join kr.taskList t where o.id = :objectiveId and o.isClosed = false")
@@ -21,7 +20,6 @@ public interface ObjectiveJpaRepository extends JpaRepository<Objective, Long> {
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Objective o SET o.idx = o.idx + 1 WHERE o.user.id = :userId")
     void bulkUpdateIdxIncrease(@Param("userId") Long userId);
-    Long countAllByUserId(Long userId);
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Objective o SET o.idx = o.idx + 1 WHERE o.idx >= :lBound AND o.idx < :uBound AND o.user.id = :userId AND o.id != :targetId")
     void bulkUpdateIdxIncrease(@Param("lBound") int lowerBound, @Param("uBound") int upperBound, @Param("userId") Long userId, @Param("targetId") Long targetId);
@@ -32,5 +30,6 @@ public interface ObjectiveJpaRepository extends JpaRepository<Objective, Long> {
     @Query("UPDATE Objective  o SET o.idx = o.idx - 1 WHERE o.idx >= :lBound AND o.user.id = :userId")
     void bulkUpdateIdxDecreaseAfter(@Param("lBound") int lowerBound, @Param("userId") Long userId);
     List<Objective> findAllByUserIn(List<User> userList);
+    Long countAllByUserId(Long userId);
 
 }
