8668a6d0ad9c0ed5503f7796b49290934e2153f6
Choi YoungLyn <joeuni0129@gmail.com>
2024-07-30 00:51:10 +0900

    [Fix] #297 - DiscordConstants 삭제 후 재연결

--- moonshot-common/src/main/java/org/moonshot/constants/DiscordConstants.java
+++
@@ -1,7 +0,0 @@
-package org.moonshot.constants;
-
-public class DiscordConstants {
-
-    public static final String signInWebhookUrl = "https://discord.com/api/webhooks/1197531070241968261/h18e24N_r18WGafR6syjSVX8568oO6grRY6jt21V2_vy-LVuy47jVUmMnB-sTFx3CZ-S";
-
-}
